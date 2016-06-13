fd723d5c762df5a6c256977117f854b7bb91c1ec
Andrew <andrewday40@gmail.com>
2016-06-13 17:55:31 -0400

    added in webpages

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 var express = require('express');
 var server = express ();
+
+server.get('/', function(request, response){
+  response.sendFile('public/html/index.html', {root: __dirname});
+});
+
+
+
+server.listen(8080);
