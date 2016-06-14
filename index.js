8330547e658c3e1d4fe7d3acb389e6b2942c7f2c
Andrew <andrewday40@gmail.com>
2016-06-14 17:55:13 -0400

    minor changes

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var express = require('express');
-var server = express ();
+var server = express();
+
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
 });
 
+server.get('/about', function(request, response){
+  response.sendFile('public/html/about.html', {root: __dirname});
+});
+
+server.get('/content', function(request, response){
+  response.sendFile('public/html/content.html', {root: __dirname});
+});
 
 
 server.listen(8080);
