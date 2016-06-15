6fe75bf2b142d7a3fc9bdae35b92e588fe1a35fe
Andrew <andrewday40@gmail.com>
2016-06-14 20:42:24 -0400

    added in comments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var server = express();
 
 server.use(express.static(__dirname+'/public'));
-// Command to add the location for pictures in the server. 
+// Command to add the location for pictures in the server.
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
 });
@@ -14,6 +14,6 @@ server.get('/about', function(request, response){
 server.get('/content', function(request, response){
   response.sendFile('public/html/content.html', {root: __dirname});
 });
-
+// Commands for finding and getting to the individual.
 
 server.listen(8080);
