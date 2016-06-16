52bf3e977d4acd8253987a862e242cc652e7a37b
Andrew <andrewday40@gmail.com>
2016-06-16 16:28:57 -0400

    added form to contact page and added needed js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var server = express();
+var bodyParser = require('body-parser');
 
 server.use(express.static(__dirname+'/public'));
 // Command to add the location for pictures in the server.
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
 });
@@ -15,5 +19,9 @@ server.get('/content', function(request, response){
   response.sendFile('public/html/content.html', {root: __dirname});
 });
 // Commands for finding and getting to the individual.
+
+server.post('/about', function(request, response){
+  response.json(request.body);
+});
 
 server.listen(8080);
